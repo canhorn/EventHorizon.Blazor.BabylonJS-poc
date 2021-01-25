0b33198d2054d9be8bd674650726a8039f06b738
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-01-24 19:41:07 -0600

    Updated Login/Authorization Workflow It will now better manage the user account state for the pages of the Editor. Updated the Home page and added more details on what to do. Updated DIsplayLoginStatus with text overrides.
    Created Platform User Provider HOC.
    This allows for the flow management for checking a users authorization details.
    
    Updated formatting on Zone Selection display.

---
+++ EventHorizon.Game.Editor/Client/Authentication/Model/PlatformUserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EventHorizon.Game.Editor.Client.Authentication.Model
+{
+    public class PlatformUserModel
+    {
+        public bool IsAuthenticated { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
