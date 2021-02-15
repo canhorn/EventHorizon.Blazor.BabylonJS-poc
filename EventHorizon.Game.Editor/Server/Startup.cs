720b3fd3a26dc00bbef2c19207658f34b6d88e16
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-02-15 14:25:16 -0600

    Fixed watch reload. Using ResponseCompression conflicts with watch injected code causing issues with response rewriting.

--- EventHorizon.Game.Editor/Server/Startup.cs
+++ EventHorizon.Game.Editor/Server/Startup.cs
@@ -38,10 +38,10 @@ namespace EventHorizon.Game.Editor.Server
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseResponseCompression();
             }
 
             app.UseHttpsRedirection();
-            app.UseResponseCompression();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles(
                 GetStaticFileOptions(
