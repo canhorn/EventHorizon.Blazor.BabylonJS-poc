b4c7e94c00882b0235b9e557704336a87fa24a05
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-03-12 22:01:45 -0600

    Application Version Details Added version details across Core, Zone and Editor. Exposed Zone Version on Editor Zone Selection/Details.
    Core Application Version
    Enriched Log details with ApplicationVersion.
    Updated appsettings.json, Dockerfile, azure-pipeline.yml with version details.
    
    Zone Application Version
    Enriched Log details with ApplicationVersion.
    Updated appsettings.json, Dockerfile, azure-pipeline.yml with version details.
    
    General Formatting and Styles.
    
    2FC8FF3D-BA5E-4AE2-9335-8B0B64FB5E8A

--- EventHorizon.Game.Editor/Core/EventHorizon.Game.Editor.Core.Api/Services/Model/CoreZoneDetails.cs
+++ EventHorizon.Game.Editor/Core/EventHorizon.Game.Editor.Core.Api/Services/Model/CoreZoneDetails.cs
@@ -7,7 +7,10 @@
             "serverAddress": "https://zone-server-93e462ba-ac14-4be3-b578-002424d1743c.local.ehzgames.studio",
             "connectionId": null,
             "tag": "home",
-            "lastPing": "2020-11-17T04:29:12.1812062+00:00"
+            "lastPing": "2020-11-17T04:29:12.1812062+00:00",
+            "details": {
+                "applicationVersion": "0.0.0-dev"
+            }
      */
     public class CoreZoneDetails
     {
@@ -15,5 +18,6 @@
         public string ServerAddress { get; set; } = string.Empty;
         public string Tag { get; set; } = string.Empty;
         public DateTimeOffset LastPing { get; set; }
+        public ServiceDetails Details { get; set; } = new ServiceDetails();
     }
 }
---
+++ EventHorizon.Game.Editor/Core/EventHorizon.Game.Editor.Core.Api/Services/Model/ServiceDetails.cs
@@ -0,0 +1,7 @@
+﻿namespace EventHorizon.Game.Editor.Core.Services.Model
+{
+    public class ServiceDetails
+    {
+        public string ApplicationVersion { get; set; } = string.Empty;
+    }
+}
