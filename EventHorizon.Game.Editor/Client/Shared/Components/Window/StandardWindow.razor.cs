5b02f6e16a4763ec8066c4ed975b73fd9897375b
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-05-05 23:41:25 -0500

    Draggable Window Component A modal like component with more features, mainly draggable and able to be placed around the screen.
    Updated Wizard Components
    Split out WizardList into separate component, making it easier to compose components together for window separation.
    
    Added padding to standard modal body.
    
    General Styles and Formatting.
    
    AB#79

---
+++ EventHorizon.Game.Editor/Client/Shared/Components/Window/StandardWindow.razor.cs
@@ -0,0 +1,154 @@
+﻿namespace EventHorizon.Game.Editor.Client.Shared.Components.Window
+{
+    using EventHorizon.Game.Editor.Client.Localization;
+    using EventHorizon.Game.Editor.Client.Localization.Api;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Components.Web;
+
+    public partial class StandardWindowModel
+        : ComponentBase
+    {
+        [Parameter]
+        public bool IsOpen { get; set; }
+        [Parameter]
+        public bool IsDraggable { get; set; } = false;
+        [Parameter]
+        public RenderFragment Header { get; set; } = null!;
+        [Parameter]
+        public RenderFragment Body { get; set; } = null!;
+        [Parameter]
+        public string WidthSize { get; set; } = "small";
+        [Parameter]
+        public string HeightSize { get; set; } = "small";
+        /// <summary>
+        /// Where to display the Window
+        /// 
+        /// <pre>
+        /// | TopLeft     |   TopCenter       |   TopRight    |
+        /// | MiddleLeft  |   MiddleCenter    |   MiddleRight |
+        /// | BottomLeft  |   BottomCenter    |   BottomRight |
+        /// </pre>
+        /// 
+        /// 900x300
+        /// </summary>
+        [Parameter]
+        public WindowLocationType Location { get; set; } = WindowLocationType.MiddleCenter;
+
+        [Inject]
+        public Localizer<SharedResource> Localizer { get; set; } = null!;
+        [Inject]
+        public ViewableArea ViewableArea { get; set; } = null!;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            SetupLocation();
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            SetupLocation();
+        }
+
+        #region Location
+        public WindowLocationType CurrentLocation = WindowLocationType.None;
+        public WindowLocationType InitialLocation = WindowLocationType.None;
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public (int PosX, int PosY) PositionDetails => CurrentLocation.Resolve(ViewableArea);
+
+        private void SetupLocation()
+        {
+            if (Location.Equals(InitialLocation).IsNotTrue())
+            {
+                CurrentLocation = Location;
+                InitialLocation = Location;
+                var (PosX, PosY) = PositionDetails;
+                Top = PosY;
+                Left = PosX;
+            }
+        }
+        #endregion
+
+        #region Collapse
+        public string CollapseClassName { get; private set; } = string.Empty;
+        public bool IsCollapsed { get; set; }
+        public string CollapsedIcon { get; set; } = "compress-arrows-alt";
+
+        public void HandleCollapse()
+        {
+            if (string.IsNullOrEmpty(
+                CollapseClassName
+            ))
+            {
+                IsCollapsed = true;
+                CollapseClassName = "--collapse";
+                CollapsedIcon = "expand-arrows-alt";
+                return;
+            }
+
+            IsCollapsed = false;
+            CollapseClassName = string.Empty;
+            CollapsedIcon = "compress-arrows-alt";
+        }
+
+        public void HandleOpenCollapsed()
+        {
+            if (string.IsNullOrEmpty(
+                CollapseClassName
+            ))
+            {
+                return;
+            }
+            CollapseClassName = string.Empty;
+        }
+        #endregion
+
+        #region Dragging Method
+        public bool IsDragging { get; private set; }
+
+        private double pos1 = 0;
+        private double pos2 = 0;
+        private double pos3 = 0;
+        private double pos4 = 0;
+
+        public void HandleDragMouseDown(MouseEventArgs args)
+        {
+            if (IsDraggable.IsNotTrue())
+            {
+                return;
+            }
+            pos3 = args.ClientX;
+            pos4 = args.ClientY;
+
+            IsDragging = true;
+        }
+
+        public void HandleElementDrag(MouseEventArgs args)
+        {
+            if (!IsDragging)
+            {
+                return;
+            }
+
+            pos1 = pos3 - args.ClientX;
+            pos2 = pos4 - args.ClientY;
+            pos3 = args.ClientX;
+            pos4 = args.ClientY;
+
+            Top -= pos2;
+            Left -= pos1;
+
+            CurrentLocation = $"{(int)Left}x{(int)Top}";
+        }
+
+        public void HandleStopDragging(MouseEventArgs _)
+        {
+            IsDragging = false;
+        }
+        #endregion
+    }
+}
---
+++ EventHorizon.Game.Editor/Client/Shared/Components/Window/WindowLocationType.cs
@@ -0,0 +1,107 @@
+﻿namespace EventHorizon.Game.Editor.Client.Shared.Components.Window
+{
+    using System;
+
+    public struct WindowLocationType
+    {
+        public static readonly WindowLocationType None = string.Empty;
+        public static readonly WindowLocationType TopLeft = new(
+            "TopLeft",
+            true,
+            (viewableArea) => (viewableArea.InnerWidth / 3 * 0, viewableArea.InnerHeight / 3 * 0)
+        );
+        public static readonly WindowLocationType TopCenter = new(
+            "TopCenter",
+            true,
+            (viewableArea) => (viewableArea.InnerWidth / 3 * 1, viewableArea.InnerHeight / 3 * 0)
+            );
+        public static readonly WindowLocationType TopRight = new(
+            "TopRight",
+            true,
+            (viewableArea) => (viewableArea.InnerWidth / 3 * 2, viewableArea.InnerHeight / 3 * 0)
+        );
+        public static readonly WindowLocationType MiddleLeft = new(
+            "MiddleLeft",
+            true,
+            (viewableArea) => (viewableArea.InnerWidth / 3 * 0, viewableArea.InnerHeight / 3 * 1)
+        );
+        public static readonly WindowLocationType MiddleCenter = new(
+            "MiddleCenter",
+            true,
+            (viewableArea) => (viewableArea.InnerWidth / 3 * 1, viewableArea.InnerHeight / 3 * 1)
+        );
+        public static readonly WindowLocationType MiddleRight = new(
+            "MiddleRight",
+            true,
+            (viewableArea) => (viewableArea.InnerWidth / 3 * 2, viewableArea.InnerHeight / 3 * 1)
+        );
+        public static readonly WindowLocationType BottomLeft = new(
+            "BottomLeft",
+            true,
+            (viewableArea) => (viewableArea.InnerWidth / 3 * 0, viewableArea.InnerHeight / 3 * 2)
+        );
+        public static readonly WindowLocationType BottomCenter = new(
+            "BottomCenter",
+            true,
+            (viewableArea) => (viewableArea.InnerWidth / 3 * 1, viewableArea.InnerHeight / 3 * 2)
+        );
+        public static readonly WindowLocationType BottomRight = new(
+            "BottomRight",
+            true,
+            (viewableArea) => (viewableArea.InnerWidth / 3 * 2, viewableArea.InnerHeight / 3 * 2)
+        );
+
+        public WindowLocationType(
+            string location,
+            bool gridLocation,
+            Func<ViewableArea, (int PosX, int PosY)> resolve
+        )
+        {
+            Location = location;
+            GridLocation = gridLocation;
+            Resolve = resolve;
+        }
+
+        public string Location { get; }
+        public bool GridLocation { get; }
+        public Func<ViewableArea, (int PosX, int PosY)> Resolve { get; }
+
+        public static implicit operator WindowLocationType(
+            string location
+        ) => new(
+            location,
+            false,
+            viewableArea =>
+            {
+                if (location.Contains(
+                    "x",
+                    StringComparison.InvariantCultureIgnoreCase
+                ))
+                {
+                    var splitLocation = location
+                        .ToLowerInvariant()
+                        .Split(
+                            "x"
+                        );
+                    if (splitLocation.Length == 2)
+                    {
+                        var validPosX = int.TryParse(
+                            splitLocation[0],
+                            out var posX
+                        );
+                        var validPosY = int.TryParse(
+                            splitLocation[1],
+                            out var posY
+                        );
+
+                        if (validPosX && validPosY)
+                        {
+                            return (posX, posY);
+                        }
+                    }
+                }
+                return (0, 0);
+            }
+        );
+    }
+}
