db01c9dce943889bbbe895ed2849eea51a206c5c
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-08-07 00:26:33 -0500

    Disabled wait on Service Worker Install. This should make it update the application when a new version is installed.

--- EventHorizon.Game.Editor/Client/wwwroot/service-worker.published.js
+++ EventHorizon.Game.Editor/Client/wwwroot/service-worker.published.js
@@ -13,12 +13,13 @@ const offlineAssetsExclude = [/^service-worker\.js$/, /^appsettings\.json$/, /^v
 
 async function onInstall(event) {
     console.info('Service worker: Install');
+    self.skipWaiting();
 
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash }));
+        .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
