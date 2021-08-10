68e902c458898183deca9ea6a84a2aa52709d429
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-08-09 22:03:37 -0500

    Cleanup of dead code for Zone Game Asset.
    AB#138 AB#248

--- EventHorizon.Game.Editor/Client/AssetManagement/Pages/Zone/CreateZoneGameAssetPage.razor.cs
+++ EventHorizon.Game.Editor/Client/AssetManagement/Pages/Zone/CreateZoneGameAssetPage.razor.cs
@@ -1,11 +1,8 @@
 ﻿namespace EventHorizon.Game.Editor.Client.AssetManagement.Pages.Zone
 {
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
     using EventHorizon.Game.Editor.Client.Shared.Components;
-    using EventHorizon.Game.Editor.Client.Shared.Components.Containers;
-    using EventHorizon.Game.Editor.Client.Shared.Components.Select;
+    using EventHorizon.Game.Editor.Client.Shared.Toast.Model;
     using EventHorizon.Zone.Systems.ClientAssets.Create;
     using EventHorizon.Zone.Systems.ClientAssets.Model;
     using Microsoft.AspNetCore.Components;
@@ -17,80 +14,21 @@
         public NavigationManager NavigationManager { get; set; } = null!;
 
         protected ClientAsset Model { get; private set; } = new ClientAsset();
-        protected StandardSelectOption AssetTypeOption { get; private set; } = new();
-
-        protected ComponentState MessageState { get; private set; } = ComponentState.Loading;
-        protected string Message { get; private set; } = string.Empty;
-
-        protected ElementReference AssetNameInput { get; set; }
-
-        protected List<StandardSelectOption> AssetTypeOptions { get; set; } = new();
-
-        protected override void OnInitialized()
-        {
-            base.OnInitialized();
-
-            AssetTypeOptions = new List<StandardSelectOption>
-            {
-                new StandardSelectOption
-                {
-                    Value = string.Empty,
-                    Text = Localizer["Select a Type"],
-                    Hidden = true,
-                    Disabled = true,
-                },
-                new StandardSelectOption
-                {
-                    Value = "SCRIPT",
-                    Text = Localizer["Script"],
-                },
-                new StandardSelectOption
-                {
-                    Value = "MESH",
-                    Text = Localizer["Mesh"],
-                },
-                new StandardSelectOption
-                {
-                    Value = "IMAGE",
-                    Text = Localizer["Image"],
-                },
-                new StandardSelectOption
-                {
-                    Value = "DIALOG",
-                    Text = Localizer["Dialog"],
-                },
-            };
-
-            AssetTypeOption = AssetTypeOptions.First();
-
-            MessageState = ComponentState.Content;
-        }
-
-        protected void HandleAssetTypeChanged(
-            StandardSelectOption option
-        )
-        {
-            AssetTypeOption = option;
-            Model.Type = option.Value;
-            // TODO: Default Data based on Type selected
-        }
 
         protected async Task HandleSave()
         {
-            MessageState = ComponentState.Loading;
             var result = await Mediator.Send(
                 new CreateClientAssetCommand(
                     Model
                 )
             );
             if (!result)
-            {
-                Message = Localizer[
-                    "Failed to Create Game Asset: {0}",
-                    result.ErrorCode 
-                        ?? "ERROR"
-                ];
-                MessageState = ComponentState.Error;
+            { 
+                await ShowMessage(
+                    Localizer["Game Asset Error"],
+                    Localizer["Failed to Create Client Asset."],
+                    MessageLevel.Error
+                );
                 return;
             }
 
