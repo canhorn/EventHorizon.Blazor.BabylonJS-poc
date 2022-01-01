d2da04bec6cc1d2e27257fc72545f0885a9ffc77
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2021-12-31 21:39:13 -0600

    testing: Added Filter to Automation test script This will make it quicker for testing Release Pipelines.
    AB#538 AB#540

--- Testing/EventHorizon.Game.Editor.Automation/run-test.sh
+++ Testing/EventHorizon.Game.Editor.Automation/run-test.sh
@@ -4,4 +4,6 @@ set -e
 
 dotnet test --configuration $BUILD_CONFIGURATION \
     --no-restore --no-build \
-    --logger:"trx;LogFilePrefix=TestResult" --results-directory /TestResults/Reports /nodeReuse:false /maxCpuCount:6
+    --filter Name~Displays_Login_Page_When_Opening_Home_Page_On_Fresh_Window \
+    --logger:"trx;LogFilePrefix=TestResult" --results-directory /TestResults/Reports \
+    /nodeReuse:false /maxCpuCount:6
