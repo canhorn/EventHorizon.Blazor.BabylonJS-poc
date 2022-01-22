131b4ed515b77c550915d1f10080cb53435f03a3
Cody Merritt Anhorn <cody.anhorn@hotmail.com>
2022-01-22 14:19:20 -0600

    test: Added better open logic to TreeView open action. 🚨 This was causing intermittent error where the double click would close the tree node. fix: Removed the unused libraries for BabylonJS that are not currently used. 🐛
    NO_AB

--- Testing/EventHorizon.Game.Editor.Automation/Components/TreeView/TreeViewComponent.cs
+++ Testing/EventHorizon.Game.Editor.Automation/Components/TreeView/TreeViewComponent.cs
@@ -12,19 +12,24 @@ public class TreeViewComponent<TOwner> : Control<TOwner>
     {
         [Selector("tree-node-link")]
         public Link<TOwner> Link { get; private set; }
+
         [Selector("tree-node-text")]
         public Text<TOwner> Text { get; private set; }
 
-        public ControlList<
-            TreeNodeItem,
-            TOwner
-        > Children
-        { get; private set; }
+        public ControlList<TreeNodeItem, TOwner> Children { get; private set; }
 
         public TreeNode Open()
         {
-            Link.Click();
-            Link.Click();
+            if (Link.Attributes["aria-expanded"] == "false")
+            {
+                Link.Click();
+            }
+
+            // Check again, edge case where Atata is faster than Loading finished.
+            if (Link.Attributes["aria-expanded"] == "false")
+            {
+                Link.Click();
+            }
 
             return this;
         }
